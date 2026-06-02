cd04a22d378bc801fef1af1d57c16bf89b3ec5b8
Pavel <yashchenkodev@gmail.com>
2026-06-03 01:30:15 +0300

    fix: render

--- js/app.js
+++ js/app.js
@@ -4,9 +4,10 @@ const tasksContainer = document.querySelector('.tasks')
 let tasks = []
 let taskId = 0
 
-inputAddButton.addEventListener('click', getInputValue)
+inputAddButton.addEventListener('click', addTask)
 
-function getInputValue() {
+
+function addTask() {
   const inputValue = input.value.trim()
 
   if (inputValue === '') {
@@ -25,24 +26,25 @@ function getInputValue() {
 }
 
 function renderTask(array) {
-  const newTask = document.createElement('div')
-
+  tasksContainer.innerHTML = ''
+  
   array.forEach((item) => {
+    const newTask = document.createElement('div')
+    newTask.classList.add('task');
+    newTask.dataset.id = item.id;
+    
     newTask.innerHTML = `
-      <div class="task">
-        <label class="task-label">
-          <input class="task-checkbox-input" type="checkbox" />
-          <span class="task-checkbox"></span>
-          <span class="task-name">${item.text}</span>
-        </label>
-        <button class="task-close">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-          </svg>
-        </button>
-      </div>
+      <label class="task-label">
+        <input class="task-checkbox-input" type="checkbox" />
+        <span class="task-checkbox"></span>
+        <span class="task-name">${item.text}</span>
+      </label>
+      <button class="task-close">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+        </svg>
+      </button>
     `
+    tasksContainer.append(newTask)
   })
-
-  tasksContainer.append(newTask)
 }
